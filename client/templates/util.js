06409884ecc3b167ca80ef7d040ad9935d198d51
Swen Jung <swen.jung@psx-technology.com>
2015-08-26 23:14:42 +0200

    added acl for editing requiring logged in user

--- client/templates/util.js
+++ client/templates/util.js
@@ -13,3 +13,11 @@ Template.registerHelper('gte', function(base, compare){
 Template.registerHelper('lte', function(base, compare){
     return base <= compare;
 });
+
+Template.registerHelper('isUserLoggedIn', function(){
+   return Meteor.user() != null;
+});
+
+Template.registerHelper('isNotUserLoggedIn', function(){
+    return Meteor.user() === null;
+});
